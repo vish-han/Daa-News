6440cc3ac030168e25f940cf540b9d0d3ef0c9b2
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-01-22 22:30:21 +0530

    prev and next button added

--- src/components/News.js
+++ src/components/News.js
@@ -266,33 +266,74 @@ export default class News extends Component {
     super();
     console.log("this is a constructor");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
     };
+  }
+
+  componentDidMount = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d707098271244ee5b4ea757fc4c7bc2f`;
+    let ParsedData = await fetch(url);
+    let data = await ParsedData.json();
+    this.setState({
+      articles: data.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("next");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d707098271244ee5b4ea757fc4c7bc2f&page=${
+      this.state.page + 1
+    }`;
+    let ParsedData = await fetch(url);
+    let data = await ParsedData.json();
+    this.setState({
+      articles: data.articles,
+      page: this.state.page + 1,
+    });
+  };
+  handlePrevClick() {
+    console.log("prev");
   }
 
   render() {
     return (
-      <div className="container ">
-        <h2 className="text-center fs-40 mb-2 display-1">Top Headlines</h2>
+      <>
+        <div className="container ">
+          <h2 className="text-center fs-40 mb-2 mt-10 display-1">
+            Top Headlines
+          </h2>
 
-        <div className="row">
-          {this.state.articles.map((news) => {
-            return (
-              <div className="col-md-3" key={news.url}>
-                <NewsItem
-                  title={news.title ? news.title.slice(0, 45) : ""}
-                  description={
-                    news.description ? news.description.slice(0, 88) : ""
-                  }
-                  src={news.urlToImage}
-                  newsURL={news.url}
-                />
-              </div>
-            );
-          })}
+          <div className="row">
+            {this.state.articles.map((news) => {
+              return (
+                <div className="col-md-4" key={news.url}>
+                  <NewsItem
+                    title={news.title ? news.title.slice(0, 45) : ""}
+                    description={
+                      news.description ? news.description.slice(0, 88) : ""
+                    }
+                    src={news.urlToImage}
+                    newsURL={news.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button
+              disabled={this.state.page <= 1}
+              className="btn btn-dark"
+              onClick={this.handlePrevClick}
+            >
+              &larr; Previous{" "}
+            </button>
+            <button className="btn btn-dark" onClick={this.handleNextClick}>
+              Next &rarr;
+            </button>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-
   render() {
-    return( 
-        <div className="card my-3 m-5" style={{width: "18rem"}}>
-  <img src={!this.props.src?"https://images.livemint.com/img/2022/01/22/600x338/Election_Commission_1642819354907_1642819355043.jpg":this.props.src} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{(this.props.title)}...</h5>
-    <p className="card-text">{(this.props.description)}...</p>
-    <a href={this.props.newsURL} className="btn btn-outline-primary">Read More</a>
-  </div>
-</div>);
+    return (
+      <div className="card my-3 m-5" style={{ width: "18rem" }}>
+        <img
+          src={
+            !this.props.src
+              ? "https://images.livemint.com/img/2022/01/22/600x338/Election_Commission_1642819354907_1642819355043.jpg"
+              : this.props.src
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.title}...</h5>
+          <p className="card-text">{this.props.description}...</p>
+          <a href={this.props.newsURL} className="btn btn-outline-dark">
+            Read More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
